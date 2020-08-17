23dd38989176a0f61137d37dbe642a753e1e461e
yasuyuky <yasuyuki.ymd@gmail.com>
2020-08-17 22:14:42 +0900

    Show paths

--- src/main.rs
+++ src/main.rs
@@ -219,13 +219,14 @@ fn init_targets(base: &Path, targets: &[PathBuf]) -> Result<()> {
     Ok(())
 }
 
-fn read_content(path: &Path) -> Result<(Vec<String>, String)> {
+fn read_content(path: &Path) -> Result<(Vec<String>, String, String)> {
     let mut f = fs::File::open(path)?;
     let meta = f.metadata()?;
     let mut buf = String::new();
     let date = format!("{}", DateTime::<Local>::from(meta.modified()?));
     f.read_to_string(&mut buf)?;
-    Ok((buf.lines().map(String::from).collect(), date))
+    let ps = path.as_os_str().to_str().unwrap_or_default();
+    Ok((buf.lines().map(String::from).collect(), ps.to_owned(), date))
 }
 
 fn print_diffs(base: &Path, targets: &[PathBuf]) -> Result<()> {
@@ -243,9 +244,9 @@ fn print_diffs(base: &Path, targets: &[PathBuf]) -> Result<()> {
                         println!("{}", &link);
                     }
                 } else {
-                    let (srcs, srcd) = read_content(&link.source)?;
-                    let (tgts, tgtd) = read_content(&link.target)?;
-                    let diff = difflib::unified_diff(&srcs, &tgts, "src", "dst", &srcd, &tgtd, 3);
+                    let (srcs, sp, srcd) = read_content(&link.source)?;
+                    let (tgts, tp, tgtd) = read_content(&link.target)?;
+                    let diff = difflib::unified_diff(&srcs, &tgts, &sp, &tp, &srcd, &tgtd, 3);
                     for line in &diff {
                         println!("{}", line.trim_end());
                     }
