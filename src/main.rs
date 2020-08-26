2408696efe2b1561b810e5337d13126dba6f8482
yasuyuky <yasuyuki.ymd@gmail.com>
2020-08-26 23:43:38 +0900

    Test custom dest

--- src/main.rs
+++ src/main.rs
@@ -141,7 +141,7 @@ fn test_get_dest() -> Result<()> {
     let test_src = PathBuf::from("test/repo/bash/.bashrc");
     let dest = get_dest(&test_src)?;
     println!("dest: {:?}", dest);
-    assert!(dest == dirs::home_dir().unwrap());
+    assert!(dest == PathBuf::from("test/home"));
     Ok(())
 }
 
