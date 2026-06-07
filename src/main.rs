8c2ceb1e8604fd49bfc7e3d495f44d4ada2b312a
yasuyuky <yasuyuki.ymd@gmail.com>
2026-06-07 17:48:30 +0900

    fix: preserve color escape sequences in logs

--- src/main.rs
+++ src/main.rs
@@ -169,6 +169,7 @@ fn init_tracing() {
         .with_max_level(tracing::Level::INFO)
         .with_level(false)
         .with_target(false)
+        .with_ansi_sanitization(false)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 }
