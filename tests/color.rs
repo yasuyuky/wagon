0b5b2a3d24433f2f7f102cded60d271a0afdec05
yasuyuky <yasuyuki.ymd@gmail.com>
2026-06-07 18:05:44 +0900

    test: cover sanitized color output paths

--- tests/color.rs
+++ tests/color.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::process::Command;
 
 fn contains(bytes: &[u8], needle: &[u8]) -> bool {
@@ -15,4 +16,26 @@ fn color_output_keeps_escape_sequences() {
     let output = [output.stdout, output.stderr].concat();
     assert!(contains(&output, b"\x1b[1mba"));
     assert!(!contains(&output, br"\x1b"));
+}
+
+#[test]
+fn color_output_sanitizes_repo_paths() {
+    let base = std::env::temp_dir().join(format!("wagon-color-test-{}", std::process::id()));
+    let _ = fs::remove_dir_all(&base);
+    fs::create_dir_all(&base).expect("create temp repo");
+    fs::write(base.join("bad\x1b]2;owned\x07"), "").expect("write temp file");
+
+    let output = Command::new(env!("CARGO_BIN_EXE_wagon"))
+        .args(["--color", "--base"])
+        .arg(&base)
+        .arg("ls")
+        .output()
+        .expect("run wagon");
+    let _ = fs::remove_dir_all(&base);
+
+    assert!(output.status.success(), "command failed: {output:?}");
+    let output = [output.stdout, output.stderr].concat();
+    assert!(contains(&output, b"\x1b["));
+    assert!(contains(&output, br"bad\x1b]2;owned\x07"));
+    assert!(!contains(&output, b"\x1b]2;owned"));
 }
